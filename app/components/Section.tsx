b3b58c537613d0a820ab6cdf8e85069812a215de
adhamito <aalmechkor2@gmail.com>
2024-10-10 21:57:25 +0100

    Update Section component with new layout and styles

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+type Props = {
+    isMinimized: boolean;
+    };
 
-const Section = () => {
+const Section:React.FC<Props> = ({ isMinimized }) => {
   return (
-    <div className='bg-[#f8f5f0] min-h-screen  py-16 px-14 rounded-e-2xl ml-1'>
-    <div className=''>
-    <h1 className='text-black font-bold text-2xl '>
-        Dashboard
-    </h1>
+    <div className="bg-[#f8f5f0] min-h-screen py-16 px-14 rounded-e-2xl ml-1">
+      <div>
+        <h1 className="text-black font-bold text-2xl">Dashboard</h1>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
