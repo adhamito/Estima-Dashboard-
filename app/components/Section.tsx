a7c7852a51e1fb4b42d86ae3f6ed417cf750b81d
adhamito <aalmechkor2@gmail.com>
2024-10-10 16:19:15 +0100

    feat(Section) add page of Section and making a style with tailwind

---
+++ app/components/Section.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div className='bg-[#f8f5f0] min-h-screen  py-16 px-14 rounded-e-2xl ml-1'>
+    <div className=''>
+    <h1 className='text-black font-bold text-2xl '>
+        Dashboard
+    </h1>
+    </div>
+</div>
+  )
+}
+
+export default Section
