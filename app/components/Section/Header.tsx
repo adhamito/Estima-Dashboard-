63e87385e49b205bb21f7b3e43e932d0ac987b8c
adhamito <aalmechkor2@gmail.com>
2024-10-11 17:19:17 +0100

    Update (Header): Improve layout and styling

--- app/components/Section/Header.tsx
+++ app/components/Section/Header.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const Header = () => {
   return (
-    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4 rounded-full">
-      <Card className="hover:bg-black hover:text-white">
+    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 rounded-full ">
+      <Card className="hover:bg-black hover:text-white m-3">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Current Balance</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">$4,836.00</div>
+          <div className="text-4xl font-bold">$4,836.00</div>
         </CardContent>
       </Card>
       <Card className="hover:bg-black hover:text-white">
@@ -17,7 +17,7 @@ const Header = () => {
           <CardTitle className="text-sm font-medium">Incoma</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">$3,814.25</div>
+          <div className="text-4xl font-bold">$3,814.25</div>
         </CardContent>
       </Card>
       <Card className="hover:bg-black hover:text-white">
@@ -25,7 +25,7 @@ const Header = () => {
           <CardTitle className="text-sm font-medium">Expenses</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">$1,700.50</div>
+          <div className="text-4xl font-bold">$1,700.50</div>
         </CardContent>
       </Card>
     </div>
