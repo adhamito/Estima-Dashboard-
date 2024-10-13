66755f33ade9e6e5658bae84f9173ca1d9d4e86b
adhamito <aalmechkor2@gmail.com>
2024-10-13 17:14:36 +0100

    Enhance Section component for cleaner layout.

--- app/components/Section/Section.tsx
+++ app/components/Section/Section.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Pots from "./Pots";
 import Budget from "./Budget";
+import TransactionsTabs from "./Tabs";
+import Biils from "./Bills";
 type Props = {
   isMinimized?: boolean;
 };
 
 const Section: React.FC<Props> = () => {
   return (
-    <div className="bg-[#f8f5f0] min-h-screen py-16 px-14 rounded-e-2xl ml-1">
-      <div className="flex flex-col justify-start items-start">
-        <h1 className="text-black font-bold text-2xl m-4">Overview</h1>
-      </div>
-      <div className="">
+    <div className="bg-[#f8f5f0] min-h-screen py-2 px-10 rounded-e-2xl ml-1">
+      <div>
         <Header />
       </div>
-      <div className="flex flex-row justify-center items-center mt-6 px-20 py-4 w-full">
-        <Pots />
-        <Budget />
+      <div className="grid grid-cols-2 gap-0 items-start w-full">
+        <div>
+          <Pots />
+          <TransactionsTabs />
+        </div>
+        <div>
+          <Budget />
+          <Biils />
+        </div>
       </div>
     </div>
   );
