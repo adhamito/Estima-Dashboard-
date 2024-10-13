00017afd5dbe28cdf9a41d52232b58f4bcab1c1a
adhamito <aalmechkor2@gmail.com>
2024-10-13 17:14:45 +0100

    Update main page for better routing and UI improvements.

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,20 @@ import SideBar from "./components/SideBar/SideBar";
 import { useState } from "react";
 
 export default function Home() {
-  // Move the useState inside the Home component
   const [isMinimized, setIsMinimized] = useState(false);
 
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full flex">
       <div
-        className="flex sm-container md-container flex-col
-       md:flex-row sm:flex-col justify-between items-start w-full"
+        className={`transition-all duration-300 ${
+          isMinimized ? "w-20" : "w-64"
+        }`}
       >
-        <div className={`w-${isMinimized ? "20" : "1/4"} h-full`}>
-          <SideBar isMinimized={isMinimized} setIsMinimized={setIsMinimized} />
-        </div>
-        <div className={`w-${isMinimized ? "full" : "full"}`}>
-          <Section isMinimized={isMinimized} />
-        </div>
+        <SideBar isMinimized={isMinimized} setIsMinimized={setIsMinimized} />
+      </div>
+
+      <div className="flex-1">
+        <Section isMinimized={isMinimized} />
       </div>
     </div>
   );
